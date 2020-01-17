adc133212acc8a22903ebb469d597ba0b68c17fe
shan0419 <shan041999@163.com>
2020-01-17 14:31:49 +0800

    49

--- App.vue
+++ App.vue
@@ -1,23 +1,10 @@
 <template>
   <div class="app">
-    <!-- v-model :value @input :date @choose-date -->
-
-    <date-picker v-model="date"/>
   </div>
 </template>
 
 <script>
-import DatePicker from './DatePicker';
-
 export default {
-  components: {
-    DatePicker,
-  },  
-  data () {
-    return {
-      date: new Date(),
-    }
-  },
 }
 </script>
 
