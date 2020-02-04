26317f5bd6750e237822b84e14de52d24af5e359
shan0419 <shan041999@163.com>
2020-02-04 20:52:44 +0800

    54

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,14 @@
 <template>
   <div id="app">
-
-    <base-level :level="2" @click="handleClick">
-
-      <!-- <template #default>
-        标题 
-      </template>
-
-      <template #header>
-        标题 
-      </template>
-
-      <div>div</div>
-      <p>p</p> -->
-
-      <template v-slot:header>
-        <p>first</p>
-      </template>
-
-      <p>second</p>
-
-    </base-level>
-
+    app
   </div>
 </template>
 
 <script>
-import BaseLevel from './components/BaseLevel';
 
 export default {
   name: 'app',
-  provide () {
-    return {
-      name: 'shanshan',
-    }
-  },
   components: {
-    BaseLevel,
-  },
-  methods: {
-    handleClick () {
-      console.log('xxxx');
-    }
   },
 }
 </script>
