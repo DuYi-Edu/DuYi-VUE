dd29e9aa4d92350c509653dead2b5f0de55d230f
shan0419 <shan041999@163.com>
2020-02-11 23:47:39 +0800

    58

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
-    <button @click="show = !show">click</button>
-    <!-- <transition-single>
-      <div v-if="show">hello world</div>
-    </transition-single>
-
-    <transition-single>
-      <div v-if="show">hello shanshan</div>
-    </transition-single> -->
-
-    <transition-single>
-      <div key="world" v-if="show">hello shanshan</div>
-      <div key="shanshan" v-else>hello world</div>
-    </transition-single>
+    app
   </div>
 </template>
 
@@ -22,12 +10,7 @@ import TransitionSingle from './components/TransitionSingle';
 export default {
   name: 'app',
   components: {
-    TransitionSingle
   },
-  data () {
-    return {
-      show: true,
-    }
-  },
+  
 }
 </script>
