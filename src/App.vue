b6fd0a02e1c8eb3c5d6d7f4347802fd5ae4ee8f7
shan0419 <shan041999@163.com>
2020-02-11 23:08:22 +0800

    56

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    app
+    <base-demo></base-demo>
   </div>
 </template>
 
 <script>
+import BaseDemo from './components/BaseDemo4';
 
 export default {
   name: 'app',
   components: {
+    BaseDemo,
   },
 }
 </script>
