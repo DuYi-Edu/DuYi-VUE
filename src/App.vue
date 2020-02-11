08d0ee9401ae03de71cad99614650cea39eedf8c
shan0419 <shan041999@163.com>
2020-02-11 23:38:50 +0800

    57

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="app">
+    <button @click="show = !show">click</button>
+    <!-- <transition-single>
+      <div v-if="show">hello world</div>
+    </transition-single>
+
+    <transition-single>
+      <div v-if="show">hello shanshan</div>
+    </transition-single> -->
+
+    <transition-single>
+      <div key="world" v-if="show">hello shanshan</div>
+      <div key="shanshan" v-else>hello world</div>
+    </transition-single>
   </div>
 </template>
 
 <script>
+import TransitionSingle from './components/TransitionSingle';
 
 export default {
   name: 'app',
   components: {
+    TransitionSingle
+  },
+  data () {
+    return {
+      show: true,
+    }
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import "velocity-animate";
-import "animate.css";
-
 Vue.config.productionTip = false
 
 new Vue({
