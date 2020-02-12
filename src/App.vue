deb5037045deafd76ed1e3c8b0bc4290d8878b69
shan0419 <shan041999@163.com>
2020-02-12 15:57:16 +0800

    59

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <template>
   <div id="app">
-    <button @click="show = true">click</button>
-    <async-cmp v-if="show"></async-cmp>
+    app
   </div>
 </template>
 
 <script>
-// import AsyncCmp from './components/AsyncCmp';
-
-// import();
 
 export default {
   name: 'app',
   components: {
-    AsyncCmp: () => import('./components/AsyncCmp'),
-  },
-  data () {
-    return {
-      show: false,
-    }
   },
 }
 </script>
--- src/components/AsyncCmp.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="async">
-    我是一个不需要在首屏加载的组件
-  </div>
-</template>
